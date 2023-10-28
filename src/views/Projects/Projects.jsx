2db4a03a1bb87a874fe2fdfdb391f6d81e2eb69b
Jorge Tolentino <hijorhe@gmail.com>
2023-10-28 00:06:19 -0600

    update

--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -60,7 +60,7 @@ const Projects = ({ darkMode }) => {
               <FiGrid />
             </h3>
           </Link> */}
-          <a href="https://drive.google.com/uc?export=download&id=1QD-O5_SU3wPTAm1dQNI0pZbVWA3ovDJZ">
+          <a href="https://drive.google.com/uc?export=download&id=1c0JfhLtL2FOrURS9wkXO4Rlu5llqxnV5">
             <h4 data-aos="fade-up">Descargar CV</h4>{" "}
             <h3 data-aos="fade-up">
               <HiOutlineFolderDownload />
